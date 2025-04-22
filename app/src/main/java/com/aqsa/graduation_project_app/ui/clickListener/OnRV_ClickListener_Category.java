bfda89c8a377a620fa0a8ccc5eb0fb6613767c80
FaresSaleemGHub <fareses11@hotmail.com>
2025-04-22 13:25:45 +0300

    Commit The Project

--- app/src/main/java/com/aqsa/graduation_project_app/ui/clickListener/OnRV_ClickListener_Category.java
+++
@@ -1,7 +0,0 @@
-package com.aqsa.graduation_project_app.ui.clickListener;
-
-import com.aqsa.graduation_project_app.model.Category;
-
-public interface OnRV_ClickListener_Category {
-    public void OnItemClick(Category category);
-}
--- app/src/main/java/com/aqsa/graduation_project_app/ui/clickListener/OnRV_ClickListener_Product.java
+++
@@ -1,7 +0,0 @@
-package com.aqsa.graduation_project_app.ui.clickListener;
-
-import com.aqsa.graduation_project_app.model.Product;
-
-public interface OnRV_ClickListener_Product {
-    public void OnItemClick(Product product);
-}
